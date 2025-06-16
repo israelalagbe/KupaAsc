298b3a05db0ce2e1a712585a259e1b3441ba6dfe
Israel Alagbe <israelalagbe53@gmail.com>
2025-06-16 22:28:22 +0100

    Added entities

---
+++ backend/src/database/entities/post.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @Column({ default: false })
+  published: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, user => user.posts, { eager: true })
+  @JoinColumn({ name: 'authorId' })
+  author: User;
+
+  @Column()
+  authorId: number;
+
+  constructor(partial: Partial<Post>) {
+    Object.assign(this, partial);
+  }
+}
---
+++ backend/src/database/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Post } from './post.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Post, post => post.author)
+  posts: Post[];
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+}
